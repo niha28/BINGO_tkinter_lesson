17b6b9cfebf65db401f05ff41a36c7aa8460fc5b
niha28 <u55773sh@gmail.com>
2021-09-13 19:19:35 +0900

    demo

---
+++ pygame_system.py
@@ -0,0 +1,41 @@
+import pygame
+import sys
+
+# 色の定義
+WHITW = (255, 255, 255)
+BLACK = (0 ,0, 0)
+
+# メイン処理をする関数
+def main():
+    # pygameモジュールの初期化
+    pygame.init()
+    pygame.display.set_caption("初めてのpygame")
+    # スクリーンの初期化
+    screen = pygame.display.set_mode((800, 600))
+    # クロックオブジェクトの作成
+    clock = pygame.time.Clock()
+    # フォントオブジェクトの作成
+    font = pygame.font.Font(None, 80)
+
+    # タイマー変数の初期化
+    tmr = 0
+
+    # ずっと
+    while True:
+        tmr = tmr + 1
+        # pygameのイベントを繰り返しで処理する
+        for event in pygame.event.get():
+            # もしバツボタンがクリックされたら
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        txt = font.render(str(tmr), True, WHITW)
+        screen.fill(BLACK)
+        screen.blit(txt, [300, 200])
+        pygame.display.update()
+        clock.tick(10)
+
+if __name__ == "__main__":
+    main()
+
+
